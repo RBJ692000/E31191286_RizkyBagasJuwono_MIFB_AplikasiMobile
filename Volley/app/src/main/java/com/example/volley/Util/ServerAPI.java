8697ee789fcab4f06ad2deee278f6fe5bcd84467
RizkyBagasJuwono <rizkyjuwono007@gmail.com>
2021-06-09 22:11:05 +0700

    E31191286

---
+++ Volley/app/src/main/java/com/example/volley/Util/ServerAPI.java
@@ -0,0 +1,8 @@
+package com.example.volley.Util;
+
+public class ServerAPI {
+    public static final String URL_DATA = "http://192.168.43.219/B_E31191286_RizkyBagasJuwono_Web/Minggu_9/CodeIgniter/Mahasiswa/Api";
+    public static final String URL_INSERT = "http://192.168.43.219/B_E31191286_RizkyBagasJuwono_Web/Minggu_9/CodeIgniter/Mahasiswa/ApiInsert";
+    public static final String URL_DELETE = "http://192.168.43.219/B_E31191286_RizkyBagasJuwono_Web/Minggu_9/CodeIgniter/Mahasiswa/ApiDelete";
+    public static final String URL_UPDATE = "http://192.168.43.219/B_E31191286_RizkyBagasJuwono_Web/Minggu_9/CodeIgniter/Mahasiswa/ApiUpdate";
+}
